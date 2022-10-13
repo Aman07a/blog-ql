bfcd0f6dedff8ce28715b19e6c56430cb5cc0ce0
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-13 20:39:19 +0200

    Added Eloquent to Topic

--- app/Topic.php
+++ app/Topic.php
@@ -2,9 +2,18 @@
 
 namespace App;
 
+use App\Post;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Topic extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'slug',
+    ];
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
