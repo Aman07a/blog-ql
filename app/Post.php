3cdd0bae709ae041e11724efd470d7844ebcf818
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-13 20:39:00 +0200

    Added Eloquent to Post

--- app/Post.php
+++ app/Post.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
+use App\User;
+use App\Topic;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    //
+    public function topic(): BelongsTo
+    {
+        return $this->belongsTo(Topic::class);
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
 }
