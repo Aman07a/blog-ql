31a9737b41b433373c9d7ceb437a3a96a5264cae
Aman Ahmed <aman16.aa17@gmail.com>
2022-10-14 10:28:24 +0200

    Updated app from resources

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,67 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import ApolloClient from 'apollo-boost';
+import moment from "moment";
+import Vue from 'vue';
+import VueApollo from 'vue-apollo';
+import VueRouter from 'vue-router';
+import AuthorPostList from './AuthorPostList';
+import './bootstrap';
+import NotFound from './NotFound';
+import Post from './Post';
+import PostList from './PostList';
+import TopicPostList from './TopicPostList';
 
-require('./bootstrap');
+window.Vue = Vue;
+Vue.use(VueRouter);
 
-window.Vue = require('vue');
+const routes = [
+    {
+        path: '/',
+        name: 'index',
+        component: PostList
+    },
+    {
+        path: '/post/:id',
+        name: 'post',
+        component: Post
+    },
+    {
+        path: '/topics/:slug',
+        name: 'topic',
+        component: TopicPostList
+    },
+    {
+        path: '/authors/:id',
+        name: 'author',
+        component: AuthorPostList
+    },
+    {
+        path: '*',
+        name: '404',
+        component: NotFound
+    }
+];
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+Vue.use(VueApollo);
+const apolloClient = new ApolloClient({
+    // You should use an absolute URL here
+    uri: 'http://127.0.0.1:8000/graphql'
+});
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+});
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+const router = new VueRouter({
+    mode: 'history',
+    routes
+});
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+
+Vue.filter("timeago", value => moment(value).fromNow());
+Vue.filter("longDate", value => moment(value).format("MMMM Do YYYY"));
 
 const app = new Vue({
     el: '#app',
+    apolloProvider,
+    router
 });
